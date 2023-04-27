e5e2665f607b1dfaa522a6816313abb2493878bf
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-04-27 21:37:17 +0200

    Update user-login-form.component.ts
    code clean and new user message
    router imported and defined

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -9,19 +9,28 @@ import { FetchApiDataService } from '../fetch-api-data.service';
 // This import is used to display notifications back to the user
 import { MatSnackBar } from '@angular/material/snack-bar';
 
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-login-form',
   templateUrl: './user-login-form.component.html',
   styleUrls: ['./user-login-form.component.scss']
 })
-export class UserLoginFormComponent {
-  @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
+export class UserLoginFormComponent implements OnInit{
+
+  @Input() userData = { 
+      Username: '',
+      Password: '',
+  };
+
 
   constructor(
       public fetchApiData: FetchApiDataService,
       public dialogRef: MatDialogRef<UserLoginFormComponent>,
-      public snackBar: MatSnackBar) { }
+      public snackBar: MatSnackBar,
+      private router: Router,
+      ) {}
+      
   
   ngOnInit(): void {
   }
@@ -35,9 +44,10 @@ export class UserLoginFormComponent {
         localStorage.setItem('token', result.token);
   // This will close the modal on success!
         this.dialogRef.close();
-       this.snackBar.open(`Welcome back ${this.userData.Username} `, 'OK', {
+        this.snackBar.open(`Welcome back ${this.userData.Username} `, 'OK', {
           duration: 2000
-       });
+        });
+        this.router.navigate(['movies']);
       }, (result) => {
         console.log(result);
         this.snackBar.open(result, 'OK', {
