063a8d35ad9b8214b178a4c8e71f052ddfff770d
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-04-27 21:26:54 +0200

    Update app.module.ts
    modules and components imported and declare

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,53 @@
+// Import from Angular packages
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+
+// Import from Angular/Material
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatIconModule } from '@angular/material/icon';
 
-import { FormsModule } from '@angular/forms';
+// Import from components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { DirectorViewComponent } from './director-view/director-view.component';
+import { GenreViewComponent } from './genre-view/genre-view.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+
+// Routes to 3 components
+const appRoutes: Routes = [
+  { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
+  { path: 'welcome', component: WelcomePageComponent },
+  {path: 'profile', component: UserProfileComponent},
+  { path: 'movies', component: MovieCardComponent },
+  ];
 
 @NgModule({
   declarations: [
     AppComponent,
+    WelcomePageComponent,
     UserRegistrationFormComponent,
     UserLoginFormComponent,
+    MovieCardComponent,
+    UserProfileComponent,
+    DirectorViewComponent,
+    GenreViewComponent,
+    NavbarComponent,
+    MovieDetailsComponent,
   ],
   imports: [
     BrowserModule,
@@ -32,7 +60,9 @@ import { UserLoginFormComponent } from './user-login-form/user-login-form.compon
     MatFormFieldModule,
     MatDialogModule,
     MatSnackBarModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
